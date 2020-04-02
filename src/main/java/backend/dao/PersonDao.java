bf67aeafc81bed6bf509a537bfc5246122e8040c
Franciszek Grobelny <franciszekgeobelny@gmail.com>
2020-04-02 11:57:56 +0200

    allInOne merge

---
+++ src/main/java/backend/dao/PersonDao.java
@@ -0,0 +1,110 @@
+package backend.dao;
+
+import backend.exceptions.NotFoundException;
+import backend.models.Person;
+import backend.services.DbConnectorService;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PersonDao {
+
+    private DbConnectorService dao;
+    private Person person;
+
+    public PersonDao()
+    {
+        dao = new DbConnectorService();
+    }
+
+    private static final String CREATE_PERSON_QUERY =
+            "INSERT INTO persons(login, password, email, anotherEmail, permission) VALUES (?,?,?,?,?);";
+    private static final String READ_PERSON_QUERY =
+            "SELECT * FROM persons where id = ?;";
+    private static final String UPDATE_PERSON_QUERY =
+            "UPDATE persons SET login=?, password=?, email=?, anotherEmail=?, permission=? WHERE id = ?;";
+    private static final String DELETE_PERSON_QUERY =
+            "DELETE FROM persons where id = ?;";
+
+    public Person create(Person person) {
+
+        try (PreparedStatement insertStatement = dao.connect().prepareStatement(CREATE_PERSON_QUERY,
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            insertStatement.setString(1, person.getLogin());
+            insertStatement.setString(2, person.getPassword());
+            insertStatement.setString(3, person.getEmail());
+            insertStatement.setString(4, person.getAnotherEmail());
+            insertStatement.setBoolean(5, person.getPermission());
+            ResultSet resultSet = insertStatement.executeQuery();
+
+            if (resultSet == null) {
+                throw new RuntimeException("Execute update returned " + resultSet);
+            }
+
+            try (ResultSet generatedKeys = insertStatement.getGeneratedKeys()) {
+                if (generatedKeys.first()) {
+                    person.setId(generatedKeys.getInt(1));
+                    return person;
+                } else {
+                    throw new RuntimeException("Generated key was not found");
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Person read(Integer personId) {
+
+        try (PreparedStatement statement = dao.connect().prepareStatement(READ_PERSON_QUERY)) {
+            statement.setInt(1, personId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    person.setId(resultSet.getInt("ID"));
+                    person.setLogin(resultSet.getString("Login"));
+                    person.setPassword(resultSet.getString("Password"));
+                    person.setEmail(resultSet.getString("Email"));
+                    person.setAnotherEmail(resultSet.getString("AnotherContact"));
+                    person.setPermission(resultSet.getBoolean("Permission"));
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return person;
+    }
+
+    public void update(Person person) {
+        try (PreparedStatement statement = dao.connect().prepareStatement(UPDATE_PERSON_QUERY)) {
+            statement.setInt(6, person.getId());
+            statement.setString(1, person.getLogin());
+            statement.setString(2, person.getPassword());
+            statement.setString(3, person.getEmail());
+            statement.setString(4, person.getAnotherEmail());
+            statement.setBoolean(5, person.getPermission());
+
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Integer personId) {
+        try (PreparedStatement statement = dao.connect().prepareStatement(DELETE_PERSON_QUERY)) {
+            statement.setInt(1, personId);
+            statement.executeUpdate();
+
+            boolean deleted = statement.execute();
+            if (!deleted) {
+                throw new NotFoundException("Product not found");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/backend/models/Person.java
@@ -0,0 +1,78 @@
+package backend.models;
+
+public class Person {
+    private int id;
+    private String login;
+    private String password;
+    private String email;
+    private String anotherEmail;
+    private boolean permission;
+
+    public Person(String login, String password, String email, String anotherEmail, boolean permission) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.anotherEmail = anotherEmail;
+        this.permission = permission;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAnotherEmail() {
+        return anotherEmail;
+    }
+
+    public void setAnotherEmail(String anotherEmail) {
+        this.anotherEmail = anotherEmail;
+    }
+
+    public boolean getPermission() {
+        return permission;
+    }
+
+    public void setPermission(boolean permission){
+        this.permission = permission;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", anotherEmail='" + anotherEmail + '\'' +
+                ", anotherEmail='" + permission + '\'' +
+                '}';
+    }
+}
+
